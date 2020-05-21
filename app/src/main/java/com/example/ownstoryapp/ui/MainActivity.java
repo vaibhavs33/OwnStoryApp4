28ad69d33ac4729d1bc2380619100ac74253a1ec
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2020-05-21 16:22:48 -0400

    This is a astronaut app

--- app/src/main/java/com/example/ownstoryapp/ui/MainActivity.java
+++ app/src/main/java/com/example/ownstoryapp/ui/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.ownstoryapp.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ownstoryapp.R;
 
@@ -33,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
                 startStory(name);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        userInput.setText("");
     }
 
     private void startStory(String name) {
--- app/src/main/java/com/example/ownstoryapp/ui/StoryActivity.java
+++ app/src/main/java/com/example/ownstoryapp/ui/StoryActivity.java
@@ -1,22 +1,32 @@
 package com.example.ownstoryapp.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.ownstoryapp.R;
+import com.example.ownstoryapp.model.Page;
+import com.example.ownstoryapp.model.Story;
+
+import java.util.Stack;
 
 public class StoryActivity extends AppCompatActivity {
 
-    TextView pageTextView;
-    ImageView pageImageView;
-    Button choiceButton1;
-    Button choiceButton2;
+    private TextView pageTextView;
+    private ImageView pageImageView;
+    private Button choiceButton1;
+    private Button choiceButton2;
+    private Stack<Integer> pageStack = new Stack<Integer>();
+
+    private String name;
+    private Story story;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +43,61 @@ public class StoryActivity extends AppCompatActivity {
         if(name == null || name.isEmpty()){
             name = "Friend";
         }
+        story = new Story();
+        loadPage(0);
+    }
 
-        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+    private void loadPage(int pageNumber){
+        pageStack.push(pageNumber);
+        final Page page = story.getPage(pageNumber);
+       Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+       pageImageView.setImageDrawable(image);
+       String pageText = getString(page.getTextId());
+       pageText = String.format(pageText, name);
+       pageTextView.setText(pageText);
+       if(page.isFinalPage()){
+           choiceButton1.setVisibility(View.INVISIBLE);
+           choiceButton2.setText(R.string.play_again_button_text);
+           choiceButton2.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                  loadPage(0);
+               }
+           });
+       }else{
+           loadButtons(page);
+       }
+    }
 
+    private void loadButtons(final Page page) {
+        choiceButton1.setVisibility(View.VISIBLE);
+        choiceButton1.setText(page.getChoice1().getTextId());
+        choiceButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               int nextPage = page.getChoice1().getNextPage();
+               loadPage(nextPage);
+            }
+        });
+        choiceButton2.setVisibility(View.VISIBLE);
+        choiceButton2.setText(page.getChoice2().getTextId());
+        choiceButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        pageStack.pop();
+        if(pageStack.isEmpty()){
+          super.onBackPressed();
+        }
+        else{
+            loadPage(pageStack.pop());
+        }
     }
 }
